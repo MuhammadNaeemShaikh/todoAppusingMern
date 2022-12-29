7882553830cd5dd6575082a7496f4f31fffc9849
Muhammad Naeem <64369381+NaeemShaikho@users.noreply.github.com>
2022-12-29 18:49:08 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,119 @@
+const express = require('express');
+const cors = require('cors')
+const mongoose = require('mongoose');
+const routes = require('./router/routes');
+const userSchema = require('./model/user');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}));
+app.use(cors());
+
+//Db Connection
+
+try {
+    mongoose.connect('mongodb://127.0.0.1:27017/myDb', {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    })
+} catch (error) {
+    console.log('Error in DB connection');
+}
+
+process.on('UnhandledRejection', error => {
+    console.log('DB error', error);
+});
+
+app.post('/todo', (req, res) => {
+    const {
+        userId,
+        input
+    } = req.body;
+    userSchema.updateOne({
+        email:userId
+    },{
+        $push:{
+            task:input
+        }
+    },
+    function (error, success) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(success);
+        }
+    })
+})
+
+// Create a schema for the todo list
+// const todoSchema = new mongoose.Schema({
+//     userId: {
+//         type: String,
+//         required: true,
+//     },
+//     input: [String],
+// });
+
+// // Create a model for the todo list
+// const Todo = mongoose.model('Todo', todoSchema);
+
+
+// app.post('/todo', (req, res) => {
+//     const {
+//         userId,
+//         input
+//     } = req.body;
+
+//     // Create a new todo
+//     const todo = new Todo({
+//         userId,
+//         input,
+//     });
+//     console.log(todo);
+//     // Save the todo to the database
+//     todo.save((error) => {
+//         if (error) {
+//             res.send(error);
+//         } else {
+//             res.send('Todo added successfully');
+//         }
+//     });
+
+// })
+
+
+
+
+app.get('/todos/:getName', (req, res) => {
+    // Get the userId from the request params
+    const userId = req.params.getName;
+
+    // Find all todos for the user
+    // userSchema.find({ userId }, (error, users) => {
+    //   if (error) {
+    //     res.send(error);
+    //   } else {
+    //     res.send(users);
+    //   }
+    // });
+
+    userSchema.find({}, function(err, users){
+        if(err){
+            console.log(err);
+        }
+        else {
+            res.json(users);
+        }
+    });
+    console.log("param user id",userId);
+});
+
+
+
+app.use('/', routes);
+
+app.listen(9000, () => {
+    console.log("Server is running")
+});
