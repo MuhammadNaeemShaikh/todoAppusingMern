902b9323fac11b02eb2e77dd34705a7255cb8834
Muhammad Naeem <64369381+NaeemShaikho@users.noreply.github.com>
2023-01-01 10:08:22 +0500

    Add files via upload

--- src/component/welcome/Welcome.js
+++ src/component/welcome/Welcome.js
@@ -5,39 +5,46 @@ import axios from 'axios';
 import { useSelector } from 'react-redux';
 
 const Welcome = () => {
-
+    
     const getName = useSelector((state)=>state.counter.email)
+    const id = useSelector((state)=>state.counter.id)
+
     const [input,setInputData] = useState('');
-    const [list,setList]=useState('');
+    const [list,setList]=useState([]);
     const [a, seta]= useState(0)
 
     const add = async () =>{
         if(input){    
             alert("posted");
             const res = await axios.post('http://localhost:9000/todo',{input,userId:getName})
-            console.log(res)
             seta(a+1);
-            console.log(a);
         }
         else{
             alert("invalid input")
         }
-        console.log('add')
-       
     }
 
+    //fetching data
     const getdata = async () => {
-        console.log("run");
-        const data = await axios.get(`http://localhost:9000/todos/${getName}`).then(data => data.data)
+        // const data = await axios.get(`http://localhost:9000/todos/${getName}`).then(data => data.data)
+        const dataComing = await axios.get(`http://localhost:9000/todos/${getName}`)
+        const data = await dataComing.data;
         setList(data)
-        console.log(data)
+        console.log("data",data)
+        console.log("list",list)
+
     }
     
     useEffect(() => {
-        console.log('useeffect')
-
-        getdata()
+       getdata()
     }, [a])
+
+    //deleting data
+    const dlt = async (e) =>{
+        const datacoming = await axios.get('http://localhost:9000/dlttodos/${}')
+        const data = await datacoming.data
+        console.log("data",data)
+    }
     
     
     
@@ -47,14 +54,14 @@ const Welcome = () => {
             <h1> Todo List </h1>
             <input type="text" required name="todo" value= {input.value} onChange={(e)=>setInputData(e.target.value)} placeholder="Add todo 👍"/>
             <i className="fa-regular fa-plus" onClick={add}></i>
-           <div>
-            {list.map((currentval,index)=>{
-                return(
-                    <div key={index}>
-                        <span>{currentval}</span>
-                    </div>
+            <div> 
+                 {list.map((currentval,index)=>{
+                 return(
+                     <div key={index}>
+                         <span>{currentval} <i class="fa-thin fa-trash" onClick={()=>dlt(index)}></i></span>
+                     </div>
                 )
-            })}
+             })} 
         </div>
         
 
