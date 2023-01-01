29e0949cb2aef9ad92e30748a137ee12b1f0a653
Muhammad Naeem <64369381+NaeemShaikho@users.noreply.github.com>
2023-01-01 10:57:50 +0500

    Delete router directory

--- router/routes.js
+++
@@ -1,9 +0,0 @@
-const routes = require('express').Router();
-const userController = require('../controller/user.controler');
-
-routes.post('/signup', userController.signup);
-routes.post('/signin', userController.signin);
-
-
-
-module.exports = routes;
